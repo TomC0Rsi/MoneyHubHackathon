af3a1d75dd8cedf627dd34321ce5d0191d5e56ab
Garnham <william.garnham@e.lloydsbanking.com>
2023-04-18 14:22:24 +0100

    confirm page skeleton

--- p3-app/src/App.js
+++ p3-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { SendAmountPage } from './pages/SendAmountPage';
 import SendReceivePage from './pages/SendReceivePage';
+import { ConfirmPage } from './pages/ConfirmPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className='App-header'>
         <SendReceivePage />
         <SendAmountPage />
+        <ConfirmPage />
       </header>
     </div>
   );
