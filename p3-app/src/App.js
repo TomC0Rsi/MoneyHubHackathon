e33e5f2cf4b67f8471d6941ba133158108998389
Josh Bogie <josh.bogie@lloydsbanking.com>
2023-04-18 15:04:23 +0100

    test

--- p3-app/src/App.js
+++ p3-app/src/App.js
@@ -4,20 +4,20 @@ import { MoneyHubProvider } from './hooks/MoneyhubProvider';
 import { config } from './config';
 import SendReceivePage from './pages/SendReceivePage';
 import { ConfirmPage } from './pages/ConfirmPage';
+import AccountReceivePage from './pages/AccountReceivePage';
 
 function App() {
   return (
-
     <MoneyHubProvider userConfig={config}>
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           <SendReceivePage />
+          <AccountReceivePage />
           <SendAmountPage />
           <ConfirmPage />
         </header>
       </div>
     </MoneyHubProvider>
-
   );
 }
 
